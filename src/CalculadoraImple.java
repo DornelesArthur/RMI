bd7f8c649b70fcbaa0537d22ce3648e8b39a32c4
Arthur Haas Dorneles <51213488+DornelesArthur@users.noreply.github.com>
2020-11-30 11:29:54 -0300

    Calculador pode fazer +,-,*,/

--- src/CalculadoraImple.java
+++ src/CalculadoraImple.java
@@ -8,8 +8,29 @@ public class CalculadoraImple extends UnicastRemoteObject implements Calculadora
 	protected CalculadoraImple() throws RemoteException{
 		super();
 	}
-	
-	public long add(long a, long b) throws RemoteException{
+	public double calc(double a, double b, char op) throws RemoteException{
+		double i = 0;
+		if(op == '+') {
+			i = this.add(a, b);
+		}else if(op == '-') {
+			i = this.sub(a, b);
+		}else if(op == '*') {
+			i = this.mult(a, b);
+		}else if(op == '/') {
+			i = this.div(a, b);
+		}
+		return i;
+	}
+	private double add(double a, double b){
 		return a+b;
+	}
+	private double sub(double a, double b){
+		return a-b;
+	}
+	private double mult(double a, double b){
+		return a*b;
+	}
+	private double div(double a, double b){
+		return a/b;
 	}
 }
